5deeb3ccc85db001f55d7b4f74a0c0fd502fc95b
YuanFang97 <yuanfang0097@gmail.com>
2019-08-31 17:35:35 +0800

    首页局部样式

--- vue-porject/src/App.vue
+++ vue-porject/src/App.vue
@@ -1,28 +1,90 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home">
+    <router-view></router-view>
+    <van-tabbar
+      v-model="active"
+      active-color="#D9232E"
+      inactive-color="#7F7F7F"
+      route
+      fixed
+      class="tabbar"
+    >
+      <van-tabbar-item
+        :icon="item.icon"
+        :to="item.path"
+        v-for="item in meun"
+        :key="item.name"
+        class="botton"
+        >{{ item.text }}</van-tabbar-item
+      >
+    </van-tabbar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from "vue";
 
+import MintUI from "mint-ui";
+Vue.use(MintUI);
+import "mint-ui/lib/style.css";
+
+import ElementUI from "element-ui";
+Vue.use(ElementUI);
+import "element-ui/lib/theme-chalk/index.css";
+
+import NutUI from "@nutui/nutui";
+Vue.use(NutUI);
+import "@nutui/nutui/dist/nutui.css";
+
+import { Tabbar, TabbarItem } from "vant";
+Vue.use(Tabbar).use(TabbarItem);
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      active: 0,
+      meun: [
+        {
+          name: "home",
+          text: "首页",
+          path: "/home",
+          icon: "wap-home"
+        },
+        {
+          name: "special",
+          text: "主题",
+          path: "/special",
+          icon: "medel-o"
+        },
+        {
+          name: "classify",
+          text: "分类",
+          path: "/classify",
+          icon: "qr"
+        },
+        {
+          name: "cart",
+          text: "购物车",
+          path: "/cart",
+          icon: "shopping-cart-o"
+        },
+        {
+          name: "my",
+          text: "我的",
+          path: "/my",
+          icon: "contact"
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.tabbar {
+  height: 54px;
+  .botton {
+    font-size: 13px;
+    background: #fafafa;
+  }
 }
 </style>
--- vue-porject/src/main.js
+++ vue-porject/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入router文件
+import router from './router'
+
+import store from './store'
+
 Vue.config.productionTip = false
 
+
 new Vue({
+  // 注入实例
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
